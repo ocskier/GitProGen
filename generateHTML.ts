3d2a89e385e5b626b08a5e9b66cca34d414637a4
ocskier <ocskier@gmail.com>
2020-01-15 21:52:56 -0500

    changing html, css, adding js for materialize

--- generateHTML.ts
+++ generateHTML.ts
@@ -36,234 +36,211 @@ const colors: ColorsType = {
 
 const generateHTML = (data: UserDataType) => {
   return `<!DOCTYPE html>
-          <html lang="en">
-            <head>
-                <meta charset="UTF-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-                <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-                <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
-                <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
-                <title>Document</title>
-                <style>
-                    @page {
-                      margin: 0;
-                    }
-                  *,
-                  *::after,
-                  *::before {
-                  box-sizing: border-box;
-                  }
-                  html, body {
-                  padding: 0;
-                  margin: 0;
-                  }
-                  html, body, .wrapper {
-                  height: 100%;
-                  }
-                  .wrapper {
-                  background-color: ${colors[data.color].wrapperBackground};
-                  padding-top: 100px;
-                  }
-                  body {
-                  background-color: white;
-                  -webkit-print-color-adjust: exact !important;
-                  font-family: 'Cabin', sans-serif;
-                  }
-                  main {
-                  background-color: #E9EDEE;
-                  height: auto;
-                  padding-top: 30px;
-                  }
-                  h1, h2, h3, h4, h5, h6 {
-                  font-family: 'BioRhyme', serif;
-                  margin: 0;
-                  }
-                  h1 {
-                  font-size: 3em;
-                  }
-                  h2 {
-                  font-size: 2.5em;
-                  }
-                  h3 {
-                  font-size: 2em;
-                  }
-                  h4 {
-                  font-size: 1.5em;
-                  }
-                  h5 {
-                  font-size: 1.3em;
-                  }
-                  h6 {
-                  font-size: 1.2em;
-                  }
-                  .photo-header {
-                  position: relative;
-                  margin: 0 auto;
-                  margin-bottom: -50px;
-                  display: flex;
-                  justify-content: center;
-                  flex-wrap: wrap;
-                  background-color: ${colors[data.color].headerBackground};
-                  color: ${colors[data.color].headerColor};
-                  padding: 10px;
-                  width: 95%;
-                  border-radius: 6px;
-                  }
-                  .photo-header img {
-                  width: 250px;
-                  height: 250px;
-                  border-radius: 50%;
-                  object-fit: cover;
-                  margin-top: -75px;
-                  border: 6px solid ${colors[data.color].photoBorderColor};
-                  box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
-                  }
-                  .photo-header h1, .photo-header h2 {
-                  width: 100%;
-                  text-align: center;
-                  }
-                  .photo-header h1 {
-                  margin-top: 10px;
-                  }
-                  .links-nav {
-                  width: 100%;
-                  text-align: center;
-                  padding: 20px 0;
-                  font-size: 1.1em;
-                  }
-                  .nav-link {
-                  display: inline-block;
-                  margin: 5px 10px;
-                  }
-                  .workExp-date {
-                  font-style: italic;
-                  font-size: .7em;
-                  text-align: right;
-                  margin-top: 10px;
-                  }
-                  .container {
-                  padding: 50px;
-                  padding-left: 100px;
-                  padding-right: 100px;
-                  }
-
-                  .row {
-                    display: flex;
-                    flex-wrap: wrap;
-                    justify-content: space-between;
-                    margin-top: 20px;
-                    margin-bottom: 20px;
-                  }
-
-                  .card {
-                    padding: 20px;
-                    border-radius: 6px;
-                    background-color: ${colors[data.color].headerBackground};
-                    color: ${colors[data.color].headerColor};
-                    margin: 20px;
-                  }
-                  
-                  .col {
-                  flex: 1;
-                  text-align: center;
-                  }
-
-                  a, a:hover {
-                  text-decoration: none;
-                  color: inherit;
-                  font-weight: bold;
-                  }
-
-                  @media print { 
-                    body { 
-                      zoom: .75; 
-                    } 
-                  }
-                </style>
-            </head>
-            <body>
-              <!-- profile-page-header -->
-              <div id="profile-page-header" class="card">
-                <div class="card-image waves-effect waves-block waves-light">
-                  <!-- <img class="activator" src="" alt="user background"> -->
-                </div>
-                <figure class="card-profile-image">
-                  <img width=200 height=200 src=${
-                    data.pic
-                  } alt="profile image" class="circle z-depth-2 responsive-img activator gradient-45deg-light-blue-cyan gradient-shadow">
-                  <div style="top: 10%;right: 3%;position: absolute;"></div>
-                </figure>
-                <figure class="card-profile-image">
-                  <img width=300 height=300 src=https://maps.googleapis.com/maps/api/staticmap?center=${
-                    data.location
-                  }&zoom=14&size=400x400&key=${
-    process.env.CLIENT_ID
-  } alt="map location" class="z-depth-2 responsive-img activator gradient-45deg-light-blue-cyan 
-                  gradient-shadow">
-                  <div style="top: 10%;right: 93%;position: absolute;"></div>
-                </figure>
-                <div class="card-content">
-                  <div class="row pt-2">
-                    <div class="col s12 center m3 offset-m2">
-                      <p class="medium-small black-text">Username</p>
-                      <h4 class="card-title grey-text text-darken-4">${
-                        data.login
-                      }</h4>
-                    </div>
-                    <div class="col s12 m2 center-align">
-                      <p class="medium-small black-text">Repos</p>
-                      <h4 class="card-title grey-text text-darken-4">${
-                        data.numRepos
-                      }</h4>
-                    </div>
-                    <div class="col s12 m2 center-align">
-                      <p class="medium-small black-text">GH Link</p>
-                      <h4 class="card-title grey-text text-darken-4">${
-                        data.profile
-                      }</h4>
-                    </div>
-                    <div class="col s12 m1 right-align">
-                      <a class="btn-floating activator waves-effect waves-light rec accent-2 right">
-                        <i class="material-icons">perm_identity</i>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div class="row card-reveal">
-                  <div class="col s12 m5">
-                    <p>
-                      <span class="card-title grey-text text-darken-4">${
-                        data.login
-                      }
-                      </span>
-                      <br>
-                      <span>
-                        <i class="material-icons cyan-text text-darken-2">perm_identity</i> Teacher</span>
-                    </p>
-                    <i class="card-title material-icons right" style="position:absolute;top:5%;right:2%">close</i>
-                    <p>
-                      <i class="material-icons cyan-text text-darken-2">perm_phone_msg</i> +1 (555) 555 8989</p>
-                    <p>
-                      <i class="material-icons cyan-text text-darken-2">email</i> mail@domain.com</p>
-                    <p>
-                      <i class="material-icons cyan-text text-darken-2">cake</i> 18th June 1990</p>
-                  </div>
-                  <div class="col s12 m6">
-                    <br>
-                      <div class="input-field col s12 m6">
-                          <select id="change-subj-field" class="icons">
-                            <option data-icon="images/sample-1.jpg" disabled selected>Change Subject</option>
-                          </select>
-                      </div>
-                      <br>
-                      <button type="submit" class="btn btn-default blue-grey darken-1" id="submit-change-text">Submit Change</button>
-                  </div>
-                </div>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <link
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        rel="stylesheet"
+      />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
+      />
+      <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+      />
+      <link
+        href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap"
+        rel="stylesheet"
+      />
+      <title>Document</title>
+      <style>
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+        }
+        html,
+        body,
+        .wrapper {
+          height: 100%;
+        }
+        .wrapper {
+          background-color: ${colors[data.color].wrapperBackground};
+          padding-top: 60px;
+        }
+        body {
+          background-color: white;
+          -webkit-print-color-adjust: exact !important;
+          font-family: "Cabin", sans-serif;
+        }
+        main {
+          background-color: #e9edee;
+          height: auto;
+          padding-top: 30px;
+        }
+        figure {
+          display: flex;
+          flex-direction: column;
+        }
+        figure img {
+          width: 200px !important;
+          border-radius: 50% !important;
+          border: 1px solid ${colors[data.color].photoBorderColor};
+        }
+        figure span {
+          text-align: center;
+          margin-top: 10px;
+          font-size: large;
+        }
+        .card {
+          max-width: 956px;
+          width: 100%;
+          margin: 0 auto;
+          max-height: 600px;
+          height: auto !important;
+        }
+        .card-image {
+          display: flex;
+          justify-content: space-between;
+        }
+        h4 {
+          font-family: "BioRhyme", serif;
+          margin: 0;
+          font-size: 1.5em;
+        }
+        a,
+        a:hover {
+          text-decoration: none;
+          color: inherit;
+          font-weight: bold;
+        }
+        @media print {
+          body {
+            zoom: 0.75;
+          }
+        }
+      </style>
+    </head>
+    <body>
+      <div class="wrapper">
+        <!-- profile-page-header -->
+        <div id="profile-page-header" class="card large">
+          <div class="card-image waves-effect waves-block waves-light">
+            <!-- <img class="activator" src="" alt="user background"> -->
+            <figure class="card-profile-image">
+              <img
+                height="150"
+                src=${data.pic}
+                alt="profile image"
+                class="circle z-depth-2 responsive-img activator gradient-45deg-light-blue-cyan gradient-shadow"
+              />
+              <span>${data.location}</span>
+            </figure>
+            <figure class="card-profile-image">
+              <img
+                height="150"
+                src="https://maps.googleapis.com/maps/api/staticmap?center=${
+                  data.location
+                }&zoom=14&size=400x400&key=${process.env.CLIENT_ID}"
+                alt="map location"
+                class="z-depth-2 responsive-img activator
+              gradient-45deg-light-blue-cyan gradient-shadow"
+              />
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="row pt-2">
+              <div class="col s12 center">
+                <h4 class="card-title grey-text text-darken-4">${data.name}</h4>
               </div>
-            </body>
-          </html>`;
+              <div class="col s12 center-align">
+                <p class="medium-small black-text">Repos</p>
+                <h4 class="card-title grey-text text-darken-4">
+                  ${data.numRepos}
+                </h4>
+              </div>
+              <div class="col s12 center-align">
+                <h4 class="card-title grey-text text-darken-4">
+                  <a
+                    href=${data.profile}
+                    class="medium-small black-text"
+                    target="blank"
+                    >${data.profile}</a
+                  >
+                </h4>
+              </div>
+              <div class="col s12 right-align">
+                <a
+                  class="btn-floating activator waves-effect waves-light rec accent-2 right"
+                >
+                  <i class="material-icons">perm_identity</i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="row card-reveal">
+            <div class="col s12 m5">
+              <p>
+                <span class="card-title grey-text text-darken-4">
+                  ${data.login}
+                </span>
+                <br />
+                <span>
+                  <i class="material-icons cyan-text text-darken-2"
+                    >perm_identity</i
+                  >
+                  Teacher</span
+                >
+              </p>
+              <i
+                class="card-title material-icons right"
+                style="position:absolute;top:5%;right:2%"
+                >close</i
+              >
+              <p>
+                <i class="material-icons cyan-text text-darken-2"
+                  >perm_phone_msg</i
+                >
+                +1 (555) 555 8989
+              </p>
+              <p>
+                <i class="material-icons cyan-text text-darken-2">email</i>
+                mail@domain.com
+              </p>
+              <p>
+                <i class="material-icons cyan-text text-darken-2">cake</i> 18th
+                June 1990
+              </p>
+            </div>
+            <div class="col s12 m6">
+              <br />
+              <div class="input-field col s12 m6">
+                <select id="change-subj-field" class="icons">
+                  <option data-icon="images/sample-1.jpg" disabled selected
+                    >Change Subject</option
+                  >
+                </select>
+              </div>
+              <br />
+              <button
+                type="submit"
+                class="btn btn-default blue-grey darken-1"
+                id="submit-change-text"
+              >
+                Submit Change
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    </body>
+  </html>`;
 };
 
 module.exports = generateHTML;
